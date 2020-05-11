4eb6fcdea9f5e7fd0751399b135772e15a5650ec
jones-sam <samjones6058@gmail.com>
2020-05-11 13:24:47 -0300

    signup

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,34 @@ const theme = createMuiTheme({
       contrastText: "#fff",
     },
   },
-  typography: {
-    useNextvariants: true,
+  spreadThis: {
+    typography: {
+      useNextvariants: true,
+    },
+    form: {
+      textAlign: "center",
+    },
+    image: {
+      margin: "20px auto 20px auto",
+    },
+    pageTitle: {
+      margin: "10px auto 10px auto",
+    },
+    textField: {
+      margin: "10px auto 10px auto",
+    },
+    button: {
+      marginTop: 20,
+      position: "relative",
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10,
+    },
+    progress: {
+      position: "absolute",
+    },
   },
 })
 
