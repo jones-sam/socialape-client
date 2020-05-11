ea8302c154eaebeaaa5b2f451a2848f02dd60ffc
jones-sam <samjones6058@gmail.com>
2020-05-10 21:29:00 -0300

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import "./App.css"
 
+// Components
+import Navbar from "./components/Navbar"
+
 // pages
 import home from "./pages/home"
 import login from "./pages/login"
@@ -11,14 +14,18 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={home} />
-          <Route exact path="/login" component={login} />
-          <Route exact path="/signup" component={signup} />
-        </Switch>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+          </Switch>
+        </div>
       </Router>
     </div>
   )
 }
+// Paused at 4:44 Scream card details
 
 export default App
