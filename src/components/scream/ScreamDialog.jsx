ecd355b00008164c9253d424c1ba637fae66b459
jones-sam <samjones6058@gmail.com>
2020-05-14 18:38:28 -0300

    reorganizing files

--- src/components/scream/ScreamDialog.jsx
+++ src/components/scream/ScreamDialog.jsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react"
 import PropTypes from "prop-types"
 import withStyles from "@material-ui/core/styles/withStyles"
-import MyButton from "../util/MyButton"
+import MyButton from "../../util/MyButton"
 import dayjs from "dayjs"
 import { Link } from "react-router-dom"
 import LikeButton from "./LikeButton"
@@ -20,7 +20,7 @@ import ChatIcon from "@material-ui/icons/Chat"
 
 // Redix
 import { connect } from "react-redux"
-import { getScream } from "../redux/actions/dataActions"
+import { getScream } from "../../redux/actions/dataActions"
 
 const styles = (theme) => ({
   ...theme.spreadThis,
